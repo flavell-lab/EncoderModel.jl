f8b8d41faba6baeac3728e164f7e290e7012688a
urlicht <jungsu93@gmail.com>
2021-11-22 21:59:04 -0500

    remove AnalysisBase.jl; remove cost.jl

--- src/EncoderModel.jl
+++ src/EncoderModel.jl
@@ -1,7 +1,8 @@
 module EncoderModel
 
-using NLopt, ForwardDiff, StatsBase, Statistics, AnalysisBase
+using FlavellBase, GalacticOptim, NLopt, ForwardDiff, StatsBase, Statistics
 include("model.jl")
+include("fit.jl")
 
 export 
     # model.jl
@@ -9,16 +10,6 @@ export
     generate_model_nl5,
     generate_model_nl5_partial,
 
-    # cost.jl
-    cost_rss,
-    cost_abs,
-    cost_mse,
-    cost_cor,
-    reg_var_L1,
-    reg_var_L2,
-    reg_L1,
-    reg_L2,
-    
     # fit.jl
     fit_model_glopt_bound,
     fit_model_glopt_bound_reg,
