51d1d7a19cc2cbb6c712c1dd4831bc1a18bc5d9d
urlicht <jungsu93@gmail.com>
2021-11-30 17:31:31 -0500

    add model nl6; fix bug with nl5 init function (incorrect percentile, 0)

--- src/EncoderModel.jl
+++ src/EncoderModel.jl
@@ -5,15 +5,19 @@ include("model.jl")
 include("fit.jl")
 
 export 
-    # model.jl
-    ewma,
-    generate_model_nl5,
-    generate_model_nl5_partial,
-    init_ps_model5,
-
     # fit.jl
     fit_model_glopt_bound,
     fit_model_glopt_bound_reg,
     fit_model_nlopt_bound,
-    fit_model_nlopt_bound_reg
+    fit_model_nlopt_bound_reg,
+
+    # model.jl
+    ewma,
+    # nl5
+    generate_model_nl5,
+    generate_model_nl5_partial,
+    init_ps_model_nl5,
+    generate_model_nl6,
+    generate_model_nl6_partial,
+    init_ps_model_nl6
 end
---
+++ src/model/model_nl6.jl
@@ -0,0 +1,123 @@
+"""
+    generate_model_nl6(xs_s, idx_splits)
+
+model nl6 function
+predictors: velocity, θh, pumping, ang vel, curcature
+
+pumping threshold term is removed
+
+Arguments
+---------
+* `xs_s`: standardized predictors array of `(n, t)`
+* `idx_splits`: list of index range for time points splits.
+e.g. `[1:800, 801:1600] for 2 videos merged with each 800 time points`
+"""
+function generate_model_nl6(xs_s, idx_splits)
+    x1 = xs_s[1,:] # velocity
+    x2 = xs_s[2,:] # θh
+    x3 = xs_s[3,:] # pumping
+    x4 = xs_s[4,:] # ang vel
+    x5 = xs_s[5,:] #curvature
+    
+    return function (ps)
+        ps[16] .+ ps[15] .* ewma(ps[14],
+            (sin(ps[1]) .* x1 .+ cos(ps[1])) .*
+            (sin(ps[2]) .* (1 .- 2 .* lesser.(x1, ps[3])) .+ cos(ps[2])) .*
+
+            (sin(ps[4]) .* x2 .+ cos(ps[4])) .*
+            (sin(ps[5]) .* (1 .- 2 .* lesser.(x2, ps[6])) .+ cos(ps[5])) .*
+
+            (sin(ps[7]) .* x3 .+ cos(ps[7])) .*
+
+            (sin(ps[8]) .* x4 .+ cos(ps[8])) .*
+            (sin(ps[9]) .* (1 .- 2 .* lesser.(x4, ps[10])) .+ cos(ps[9])) .*
+
+            (sin(ps[11]) .* x5 .+ cos(ps[11])) .*
+            (sin(ps[12]) .* (1 .- 2 .* lesser.(x5, ps[13])) .+ cos(ps[12])), idx_splits)
+    end
+end
+
+"""
+    generate_model_nl6_partial(xs_s, idx_splits, idx_valid)
+
+model nl6 partial function
+predictors: velocity, θh, pumping, ang vel, curcature
+
+pumping threshold term is removed
+
+
+Arguments
+---------
+* `xs_s`: standardized predictors array of `(n, t)`
+* `idx_splits`: list of index range for time points splits.
+e.g. `[1:800, 801:1600] for 2 videos merged with each 800 time points`
+* `idx_valid`: list of valid parameter index. e.g. model with pumping only should be [7,8,9,16,17,18]
+"""
+function generate_model_nl6_partial(xs_s, idx_splits, idx_valid)
+    x1 = xs_s[1,:] # velocity
+    x2 = xs_s[2,:] # θh
+    x3 = xs_s[3,:] # pumping
+    x4 = xs_s[4,:] # ang vel
+    x5 = xs_s[5,:] #curvature
+    
+    return function (ps_::AbstractVector{T}) where T
+        ps = zeros(T, 16)
+        ps[idx_valid] .= ps_
+        ps[16] .+ ps[15] .* ewma(ps[14],
+            (sin(ps[1]) .* x1 .+ cos(ps[1])) .*
+            (sin(ps[2]) .* (1 .- 2 .* lesser.(x1, ps[3])) .+ cos(ps[2])) .*
+
+            (sin(ps[4]) .* x2 .+ cos(ps[4])) .*
+            (sin(ps[5]) .* (1 .- 2 .* lesser.(x2, ps[6])) .+ cos(ps[5])) .*
+
+            (sin(ps[7]) .* x3 .+ cos(ps[7])) .*
+
+            (sin(ps[8]) .* x4 .+ cos(ps[8])) .*
+            (sin(ps[9]) .* (1 .- 2 .* lesser.(x4, ps[10])) .+ cos(ps[9])) .*
+
+            (sin(ps[11]) .* x5 .+ cos(ps[11])) .*
+            (sin(ps[12]) .* (1 .- 2 .* lesser.(x5, ps[13])) .+ cos(ps[12])), idx_splits)
+    end
+end
+
+"""
+    init_ps_model_nl6(xs, idx_predictor)
+
+initializes the model nl6 parameters
+predictors: velocity, θh, pumping, ang vel, curcature
+
+Arguments
+---------
+* `xs`: predictors array of `(n, t)` (not standardized)
+* `idx_predictor`: list of index for predictors. for full model, [1,2,3,4,5]. model w/o velocity: [2,3,4,5]
+"""
+function init_ps_model_nl6(xs, idx_predictor=[1,2,3,4,5])    
+    n_xs = length(idx_predictor)
+  
+    ps_0 = []
+    ps_min = []
+    ps_max = []
+    list_idx_ps = [[1,2,3], [4,5,6], [7], [8,9,10], [11,12,13]]
+    list_idx_ps_reg = [[1,2,3], [4,5,6], [7], [8,9,10], [11,12,13]]
+
+    for (i,b) = enumerate(idx_predictor)
+        if b == 3
+            push!(ps_0, [0.])
+            push!(ps_min, [-pi/2])
+            push!(ps_max, [pi/2])
+        else
+            push!(ps_0, [0., 0., mean(xs[b,:])])
+            push!(ps_min, [-pi/2, -pi/2, percentile(zstd(xs[b,:]), 5)])
+            push!(ps_max, [pi/2, pi/2, percentile(zstd(xs[b,:]), 95)])
+        end
+    end
+    ps_0 = vcat(ps_0..., [0.1, 1., 0.])
+    ps_min = vcat(ps_min..., [0.001, -2., -2.])
+    ps_max = vcat(ps_max..., [1., 2., 2.])
+    
+    
+    list_idx_ps = vcat(list_idx_ps[idx_predictor]..., [14,15,16])
+    list_idx_ps_reg = vcat(list_idx_ps_reg[idx_predictor]..., [15]) # 14: \gamma, 16: bias
+
+    ps_0, ps_min, ps_max, list_idx_ps, list_idx_ps_reg
+end
